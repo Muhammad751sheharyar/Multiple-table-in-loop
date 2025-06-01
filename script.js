3f7ec186873afbf6cfbd9cb6d370d900000e1563
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-06-01 20:36:11 +0500

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+var num1=+prompt("enter the table num");
+for(var i= 1; i<=num1;i++){
+
+document.write("<h1>" +"Table:no:"+i+"</h1><br>");
+for( var g = 1;g<=10;g++){
+    document.write(i+"x"+g+"="+ i*g +"<br>")
+
+}
+
+   
+}
