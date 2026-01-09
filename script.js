245c23a6332a843fcd6e0b412aa0a10e5939e69b
Muhammad751sheharyar <sheharyar117794@gmail.com>
2026-01-09 12:47:40 +0500

    again

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+// var num1=+prompt("enter the table num");
+// for(var i= 1; i<=num1;i++){
+
+// document.write("<h1>" +"Table:no:"+i+"</h1><br>");
+// for( var g = 1;g<=10;g++){
+//     document.write(i+"x"+g+"="+ i*g +"<br>")
+
+// }
+
+   
+// }
+
+
+
+
 var num1=+prompt("enter the table num");
-for(var i= 1; i<=num1;i++){
+for(var i= num1; i>=1;i--){
 
 document.write("<h1>" +"Table:no:"+i+"</h1><br>");
 for( var g = 1;g<=10;g++){
@@ -9,3 +24,12 @@ for( var g = 1;g<=10;g++){
 
    
 }
+
+
+
+// let val ="*";
+// let star = "";
+// for(let i = 1 ; i <= 6 ; i++){
+//     star += val;
+//     document.write(star + "<br />");
+// }   
